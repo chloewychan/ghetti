871baffcf7b748fac58380ca707b1d1b8280c6b7
chloewychan <chloe.wyc@outlook.com>
2025-07-27 13:54:11 -0400

    Chord creator

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<a href="/create">
+<a href="/chord-creator">
     <button>Create</button>
 </a>
